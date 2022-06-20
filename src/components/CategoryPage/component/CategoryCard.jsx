a510d7df9594297dd633c1c34be114c4beb5ec03
Khrystyna Peryk <khrystyna_peryk@epam.com>
2022-06-20 21:20:32 +0400

    hover cart styling

--- src/components/CategoryPage/component/CategoryCard.jsx
+++ src/components/CategoryPage/component/CategoryCard.jsx
@@ -1,10 +1,28 @@
 import React, {Component} from 'react';
+import {ReactComponent as EmptyCart} from '../../../assets/emptyCart.svg'
 import './CategoryCard.scss';
 
 class CategoryCard extends Component {
+    constructor() {
+    super()
+    this.state = {
+      emptyCartShown: false,
+    }
+  }
   render() {
     return (
-      <div className='card-container'>
+      <div 
+        className='card-container' 
+        onMouseEnter={() => this.setState({emptyCartShown: true})}
+        onMouseLeave={() => this.setState({emptyCartShown: false})}
+      >
+        {
+          this.state.emptyCartShown && (
+            <div className='card-container-emptyCart' onClick={() => console.log(this.props.name)}>
+              <div className='emptyCart'><EmptyCart /></div>
+            </div>
+          )
+        }
         <div>
           <img className='card-container-image' src={this.props.image} alt={this.props.name} />
         </div>
