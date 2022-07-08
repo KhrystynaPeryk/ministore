497e1945ce7ad419a4ff144ac734591cbe8167a5
Khrystyna Peryk <khrystyna_peryk@epam.com>
2022-07-08 12:09:42 +0400

    select outside click feature

--- src/components/Navbar/components/Select.jsx
+++ src/components/Navbar/components/Select.jsx
@@ -12,18 +12,28 @@ class Select extends Component {
       currencies: [],
       isDropdownOpen: false,
     }
+    this.box = React.createRef();
   }
+
   componentDidMount() {
     fetch('http://localhost:4000', fetchParams(GET_CURRENCIES))
     .then((response) => response.json())
     .then(currencyList => {
       this.setState({ currencies: currencyList.data.currencies });
     });
-
+    document.addEventListener('mousedown', this.handleOutsideClick);
   }
 
   handleDropdown() {
     this.setState({isDropdownOpen: !this.state.isDropdownOpen})
+  }
+
+  handleOutsideClick = (event) => {
+    if (this.box && !this.box?.current?.contains(event.target)) {
+      this.setState({isDropdownOpen: false})
+    } else {
+      this.setState({isDropdownOpen: true})
+    }
   }
 
   render() {
@@ -34,7 +44,7 @@ class Select extends Component {
             <span className='select-container-arrow'>&#711;</span> }
           </div>
           {this.state.isDropdownOpen ? (
-            <div className='select-container-dropdown'>
+            <div className='select-container-dropdown' ref={this.box}>
               {this.state.currencies.map((currency, index) => {
                 return (
                 <div className='select-container-dropdown-option' key={index} onClick={() => {
